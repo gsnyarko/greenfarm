f08e7b5c7829a3abd28006090037726815bcce51
GIDEON NYARKO <gsnyarko@gmail.com>
2020-01-03 23:21:14 +0100

    header

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import './Header.css';
+import farmPic from './Images/farmlogo.jpg';
+
+class Header extends Component {
+    render() {
+        return (<>
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#products">Products</a>
+                <a href="#contact">Contact</a>
+                <input type="text" placeholder="Search.."></input>
+                </div>
+                <div id='pic'>
+                   <img src={farmPic} alt='background'></img>
+                </div>
+                </>);
+            }
+        }
+        
+        export default Header;
+        
+        
+        
+        
+        
+        
+        
+        
+        
